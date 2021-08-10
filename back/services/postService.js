8b104cc2e230168314298bae06772f67ea51a94e
farias <fariasjunior96@gmail.com>
2021-08-10 07:51:11 -0300

    caching for post searches

--- back/services/postService.js
+++ back/services/postService.js
@@ -1,22 +1,35 @@
 const authService = require('./authService')
 const postModel = require('../models/postSchema')
 const errors = require('../errors/postErrors')
+const cacheService = new (require('./cacheService'))('posts')
+
+const allCacheKey = '[all]'
 
 const Service = {};
 
 Service.getAllAsync = async () => {
-    return await postModel.find({})
+    const cachedData = await cacheService.getAsync(allCacheKey)
+    if (cachedData) return cachedData
+
+    const searchData = await postModel.find({})
+    cacheService.setAsync(allCacheKey, searchData)
+    return searchData
 };
 
 Service.getByIdAsync = async () => {}
 
 Service.searchAsync = async (query) => {
-    return postModel.find(
+    const cachedData = await cacheService.getAsync(query)
+    if (cachedData) return cachedData
+
+    const searchData = await postModel.find(
         {
             "content": { "$regex": query, "$options": "i" }
         },
         (err, docs) => {}
     )
+    cacheService.setAsync(query, searchData)
+    return searchData
 }
 
 Service.createAsync = async (newPostData) => {
